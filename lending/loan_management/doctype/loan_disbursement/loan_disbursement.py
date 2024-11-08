a1522280a5c21686cd80d18c68a3477ed048460d
Deepesh Garg <deepeshgarg6@gmail.com>
2024-11-08 17:51:25 +0530

    fix: Advance payment for LOC loans

--- lending/loan_management/doctype/loan_disbursement/loan_disbursement.py
+++ lending/loan_management/doctype/loan_disbursement/loan_disbursement.py
@@ -144,7 +144,7 @@ class LoanDisbursement(AccountsController):
 				gle_map,
 				broken_period_interest_account,
 				self.loan_account,
-				-1 * self.bpi_amount_difference,
+				self.bpi_amount_difference,
 				_("BPI difference entry"),
 				bpi_difference_date=self.bpi_difference_date,
 			)
