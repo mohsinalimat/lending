7a1701387b9a355c6139418ae3e6a9b2d24fc8ba
Deepesh Garg <deepeshgarg6@gmail.com>
2024-12-31 21:10:48 +0530

    fix: Repost tool updates

--- lending/loan_management/doctype/loan_repayment_repost/loan_repayment_repost.py
+++ lending/loan_management/doctype/loan_repayment_repost/loan_repayment_repost.py
@@ -198,7 +198,9 @@ class LoanRepaymentRepost(Document):
 
 			repayment_doc.allocate_amount_against_demands(amounts)
 
-			if repayment_doc.repayment_type in ("Advance Payment", "Pre Payment"):
+			if repayment_doc.repayment_type in ("Advance Payment", "Pre Payment") and (
+				not self.principal_amount_paid >= self.pending_principal_amount
+			):
 				create_update_loan_reschedule(
 					repayment_doc.against_loan,
 					repayment_doc.posting_date,
