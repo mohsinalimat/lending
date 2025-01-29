93bbbd46a6379987bb191e8a6969d41a9f36506d
Nihantra Patel <nihantra@frappe.io>
2025-01-29 17:19:40 +0530

    fix: force update dpd in loan

--- lending/loan_management/doctype/loan/loan.py
+++ lending/loan_management/doctype/loan/loan.py
@@ -726,7 +726,7 @@ def update_days_past_due_in_loans(
 	)
 
 	for disbursement in disbursements:
-		if getdate(posting_date) > add_days(getdate(), -1) or force_update_dpd_in_loan:
+		if getdate(posting_date) >= add_days(getdate(), -1) or force_update_dpd_in_loan:
 			demand = get_unpaid_demands(
 				loan_name,
 				posting_date=posting_date,
