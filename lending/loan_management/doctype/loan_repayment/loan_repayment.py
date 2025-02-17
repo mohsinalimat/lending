873967e8bc5fa29b65511b40415aed6e6f1a7343
Nihantra Patel <nihantra@frappe.io>
2025-02-17 19:18:17 +0530

    fix: update testcase and other fixes

--- lending/loan_management/doctype/loan_repayment/loan_repayment.py
+++ lending/loan_management/doctype/loan_repayment/loan_repayment.py
@@ -159,6 +159,7 @@ class LoanRepayment(AccountsController):
 				"Interest Waiver",
 				"Penalty Waiver",
 				"Charges Waiver",
+				"Normal Repayment",
 			)
 			and not self.flags.from_repost
 		):
